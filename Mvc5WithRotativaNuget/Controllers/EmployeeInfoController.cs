2487a3bb8fdfee682aad953d9d8c61b70ee39a87
karlospn <karlospn@gmail.com>
2017-06-02 11:28:36 +0200

    Initial commit

---
+++ Mvc5WithRotativaNuget/Controllers/EmployeeInfoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc5WithRotativaNuget.Models;
+using Rotativa;
+
+namespace Mvc5WithRotativaNuget.Controllers
+{
+    public class EmployeeInfoController : Controller
+    {
+        private DatabaseEntities ctx;
+
+        public EmployeeInfoController()
+        {
+            ctx = new DatabaseEntities();
+        }
+
+        public ActionResult Index()
+        {
+            var employees = ctx.EmployeeInfo.ToList();
+            return View(employees);
+        }
+
+        public ActionResult PrintAllEmployees()
+        {
+            var report = new ActionAsPdf("Index");
+            return report;
+        }
+
+        public ActionResult PrintEmployeeById(int id)
+        {
+            var report = new ActionAsPdf("ExecutePrintEmployeeById", new {id = id});
+            return report;
+        }
+
+        public ActionResult ExecutePrintEmployeeById(int id)
+        {
+            var employee = ctx.EmployeeInfo.First(x => x.EmpNo == id);
+            return View(employee);
+        }
+    }
+}
---
+++ Mvc5WithRotativaNuget/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rotativa;
+
+namespace Mvc5WithRotativaNuget.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PrintPDF(string view)
+        {
+            var report = new ActionAsPdf(view);
+            return report;
+        }
+    }
+}
